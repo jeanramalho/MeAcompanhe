71b8172ee65a02926b2f6ccf10447519b5d5b900
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-13 11:43:39 -0300

    [add] - Cria arvore de views para layout do projeto

--- MeAcompanhe/View/MeAcompanheView.swift
+++ MeAcompanhe/View/MeAcompanheView.swift
@@ -32,16 +32,134 @@ class MeAcompanheView: UIView {
         return view
     }()
     
+    lazy var mainInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        return stackView
+    }()
+    
     lazy var velocityStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
+        stackView.spacing = 8
         return stackView
     }()
     
     lazy var infoStackView: UIStackView = {
-        let 
-    }
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    lazy var latiStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    lazy var longiStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    lazy var addressStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    lazy var velocityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Velocidade"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        return label
+    }()
+    
+    lazy var latiLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Latitude:"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    lazy var longiLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Longitude:"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Endereço:"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    lazy var latiInfoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "-23.2334453"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    lazy var longiInfoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "-46.1325245"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    lazy var addressInfoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Rua Silvia de Faria Marcondes, 445, Parque Fernanda"
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    lazy var velocityNumberView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = 50
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    lazy var numberVelocityLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "7.32"
+        label.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+        return label
+    }()
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -59,10 +177,43 @@ class MeAcompanheView: UIView {
     }
     
     private func setHierarchy(){
+        addSubview(headerView)
+        headerView.addSubview(headerTitleLabel)
+        
+        addSubview(infoView)
+        infoView.addSubview(mainInfoStackView)
+        
+        mainInfoStackView.addArrangedSubview(velocityStackView)
+        mainInfoStackView.addArrangedSubview(infoStackView)
+        
+        velocityStackView.addArrangedSubview(velocityLabel)
+        velocityStackView.addArrangedSubview(velocityNumberView)
+        velocityNumberView.addSubview(numberVelocityLabel)
+        
+        infoStackView.addArrangedSubview(latiStackView)
+        infoStackView.addArrangedSubview(longiStackView)
+        infoStackView.addArrangedSubview(addressLabel)
+        
+        latiStackView.addArrangedSubview(latiLabel)
+        latiStackView.addArrangedSubview(latiInfoLabel)
+        
+        longiStackView.addArrangedSubview(longiLabel)
+        longiStackView.addArrangedSubview(longiInfoLabel)
+        
+        addressStackView.addArrangedSubview(addressLabel)
+        addressStackView.addArrangedSubview(addressInfoLabel)
+        
+        addSubview(mapView)
         
     }
     
     private func setConstraints(){
-        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            headerTitleLabel
+        ])
     }
 }
